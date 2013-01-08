cfad5b440d28f7226f406442cff7d708ccaa590b
mroushdy <marwanfroushdy@gmail.com>
2013-01-08 18:59:13 +0200

    Added backbone-localStorage jam dependancy and added .gitignore file for node_modules

--- public/vendor/jam/require.config.js
+++ public/vendor/jam/require.config.js
@@ -6,6 +6,11 @@ var jam = {
             "main": "backbone.js"
         },
         {
+            "name": "backbone-localStorage",
+            "location": "../vendor/jam/backbone-localStorage",
+            "main": "main.js"
+        },
+        {
             "name": "backbone.layoutmanager",
             "location": "../vendor/jam/backbone.layoutmanager",
             "main": "backbone.layoutmanager.js"
@@ -19,9 +24,14 @@ var jam = {
             "name": "lodash",
             "location": "../vendor/jam/lodash",
             "main": "./lodash.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
         }
     ],
-    "version": "0.2.11",
+    "version": "0.2.13",
     "shim": {
         "backbone": {
             "deps": [
@@ -37,15 +47,124 @@ var jam = {
                 "lodash"
             ],
             "exports": "Backbone.LayoutManager"
+        },
+        "underscore": {
+            "exports": "_"
         }
     }
 };
 
 if (typeof require !== "undefined" && require.config) {
-    require.config({packages: jam.packages, shim: jam.shim});
+    require.config({
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone-localStorage",
+            "location": "../vendor/jam/backbone-localStorage",
+            "main": "main.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        },
+        "underscore": {
+            "exports": "_"
+        }
+    }
+});
 }
 else {
-    var require = {packages: jam.packages, shim: jam.shim};
+    var require = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone-localStorage",
+            "location": "../vendor/jam/backbone-localStorage",
+            "main": "main.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "underscore",
+            "location": "../vendor/jam/underscore",
+            "main": "underscore.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        },
+        "underscore": {
+            "exports": "_"
+        }
+    }
+};
 }
 
 if (typeof exports !== "undefined" && typeof module !== "undefined") {
