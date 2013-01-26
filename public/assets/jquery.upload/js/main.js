4f561e01f8c7f79a9c1819e5b662445da61417ea
mroushdy <marwanfroushdy@gmail.com>
2013-01-26 22:34:09 +0200

    added errors on photo upload

--- public/assets/jquery.upload/js/main.js
+++ public/assets/jquery.upload/js/main.js
@@ -19,7 +19,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: '/listings/photos/upload'
+        url: '/listings/photos/upload',
     });
 
     // Enable iframe cross-domain access via redirect option:
@@ -31,6 +31,22 @@ $(function () {
             '/cors/result.html?%s'
         )
     );
+
+    var error = false;
+    $('#fileupload').bind('fileuploadadded', function (e, data) {
+        if(error) {
+            $('.alert-error').addClass('hide');
+        }
+        $.each(data.files, function (index, file) {
+            if(file.error == "Filetype not allowed") {
+                $('#error-wrong-file-type').removeClass('hide');
+                error = true;
+            } else if(file.error == "File is too big") {
+                $('#error-file-too-large').removeClass('hide');
+                error = true;
+            }
+        });
+    });
 
     $('#fileupload').fileupload('option', {
         maxFileSize: 2000000,
@@ -53,6 +69,15 @@ $(function () {
         ]
     });
 
+    $("#next").click(function() {
+        if($('.files li').length > 0) {
+            //redirect
+        } else {
+            $('#no-images-added').modal('show');
+        }
+    });
+
+/*
     // Load existing files:
     $.ajax({
         // Uncomment the following to send cross-domain cookies:
@@ -61,8 +86,8 @@ $(function () {
         dataType: 'json',
         context: $('#fileupload')[0]
     }).done(function (result) {
-        $(this).fileupload('option', 'done')
-            .call(this, null, {result: result});
+        $(this).fileupload('option', 'done').call(this, null, {result: result});
     });
+*/
 
 });
