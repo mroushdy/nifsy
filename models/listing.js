bf2a8b1e100078259ed454d03a153f0d28dbd5d6
mroushdy <marwanfroushdy@gmail.com>
2013-02-09 18:25:31 +0200

    started adding full-text search

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
-var mongoose = require("mongoose");
-var Alleup = require('alleup');
-var alleup = new Alleup({storage : "dir", config_file: "alleup_config.json"});
+var mongoose = require("mongoose")
+  , Fulltext = require("mongoose-fulltext")
+  , Alleup = require('alleup')
+  , alleup = new Alleup({storage : "dir", config_file: "alleup_config.json"});
 
 var ListingPhotoSchema = new mongoose.Schema({
     name: String
@@ -19,17 +20,19 @@ ListingPhotoSchema.virtual('url').get(function() {
 });
 
 var ListingSchema = new mongoose.Schema({
-  title: { type: String, required: true, index: true },
+  title: { type: String, required: true, index: true, fulltext:true, searchfield:'mainsearch' },
   _owner: { type: mongoose.Schema.Types.ObjectId, index: true, required: true, ref: 'User'  },
   visible:  { type: Boolean, default: 0, required: true },
   price: { type: Number, required: true },
   condition: { type: String, required: true, enum: ['new', 'used'] },
-  brand: { type: String, required: true },
-  description: { type: String, required: true },
+  brand: { type: String, required: true, fulltext:true, searchfield:'mainsearch' },
+  description: { type: String, required: true, fulltext:true, searchfield:'mainsearch' },
   date: { type: Date, default: Date.now, required: true },
   photos: [ListingPhotoSchema], //stores photo names but without variations
   facebook_friends: [{type: String,  index:true}] //stores fb friend_ids
 });
+
+Fulltext(ListingSchema,{indexfieldname:'sindex'});
 
 var Listing = mongoose.model('Listing', ListingSchema);
 var ListingPhoto = mongoose.model('ListingPhoto', ListingPhotoSchema);
